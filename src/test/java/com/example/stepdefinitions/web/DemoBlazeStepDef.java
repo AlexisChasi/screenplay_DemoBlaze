a33f742f6dbff9e974dd1175f3f38f5dd6dc8e0f
AlexisChasi <alexis.chasi@epn.edu.ec>
2024-07-16 20:16:20 -0500

    Cambios solicitados consumo por json y scenario outline

--- src/test/java/com/example/stepdefinitions/web/DemoBlazeStepDef.java
+++ src/test/java/com/example/stepdefinitions/web/DemoBlazeStepDef.java
@@ -15,8 +15,13 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
-import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
+
+
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class DemoBlazeStepDef {
@@ -80,20 +85,21 @@ public class DemoBlazeStepDef {
     }
 
     @And("completo el formulario de pedido con los siguientes datos:")
-    public void completoElFormularioDePedidoConLosSiguientesDatos(DataTable dataTable) {
-        Map<String, String> datosPedido = dataTable.transpose().asMap(String.class, String.class);
+    public void completarFormularioDePedido(DataTable dataTable) throws IOException {
+        List<Map<String, String>> data = leerDatosDesdeJSON();
+        for (Map<String, String> row : data) {
+            String nombre = row.get("nombre");
+            String pais = row.get("pais");
+            String ciudad = row.get("ciudad");
+            String tarjeta = row.get("tarjeta");
+            String mes = row.get("mes");
+            String anio = row.get("anio");
 
-        String nombre = datosPedido.get("nombre");
-        String pais = datosPedido.get("país");
-        String ciudad = datosPedido.get("ciudad");
-        String tarjeta = datosPedido.get("tarjeta");
-        String mes = datosPedido.get("mes");
-        String año = datosPedido.get("año");
-
-        OnStage.theActorInTheSpotlight().attemptsTo(
-                CompletarFormulario.conDatos(nombre, pais, ciudad, tarjeta, mes, año)
-        );
-        screenShot();
+            OnStage.theActorInTheSpotlight().attemptsTo(
+                    CompletarFormulario.conDatos(nombre, pais, ciudad, tarjeta, mes, anio)
+            );
+            screenShot();
+        }
     }
 
     @And("confirmo el pedido")
@@ -116,6 +122,14 @@ public class DemoBlazeStepDef {
     public void screenShot(){
         byte[] evidencia = ((TakesScreenshot) BrowseTheWeb.as(theActorInTheSpotlight()).getDriver()).getScreenshotAs(OutputType.BYTES);
         this.scenario.attach(evidencia, "image/png", "evidencias");
+    }
+
+    private List<Map<String, String>> leerDatosDesdeJSON() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream("datos_compra.json")) {
+            return objectMapper.readValue(inputStream, List.class);
+        }
     }
 
 }
